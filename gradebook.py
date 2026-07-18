9b6c9f418b09eb118d2c6e3a86966d48956fb673
saranazari82 <sara.nazari1381110@gmail.com>
2026-07-18 16:46:32 +0330

    Add Gradebook methods: add_assessment, record_grade, calculate_average

--- gradebook.py
+++ gradebook.py
@@ -1,5 +1,6 @@
 import student
 import course
+import assessment
 class Gradebook :
     def __init__(self,passing_grade) :
         self.students = {}
@@ -19,3 +20,26 @@ class Gradebook :
             course = self.courses[course_code]
             student.enroll_course(course_code)
             course.add_student(student_id)
+
+    def add_assessment(self,course_code,assessment) :
+        if course_code in self.courses :
+            course = self.courses[course_code]        
+            course.add_assessment(assessment)
+
+    def record_grade(self,student_id,course_code,assessment_title,score) :
+        if student_id in self.students and course_code in self.courses :
+            if student_id not in self.grades :
+                self.grades[student_id] = {}
+            if course_code not in self.grades[student_id] :
+                self.grades[student_id][course_code] = {}
+                self.grades[student_id][course_code][assessment_title] = score
+
+    def calculate_average(self,student_id,course_code) :
+        if student_id in self.grades and course_code in self.grades[student_id] :
+            course = self.courses[course_code]
+            percentages = []
+            for title,score in self.grades[student_id][course_code].items() :
+                assessment = course.find_assessment(title)
+                percentage = assessment.calculate_percentage(score)
+                percentages.append(percentage)
+            return sum(percentages) / len(percentages)   
