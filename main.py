1bd95994de2d7ddf2c29b8ecd8b45513b93d901e
saranazari82 <sara.nazari1381110@gmail.com>
2026-07-19 15:23:36 +0330

    Complete main.py menu with all options, tested successfully

--- main.py
+++ main.py
@@ -18,20 +18,23 @@ def show_menu():
 exit = False
 while not exit :
     show_menu()
-    choice = input("Choose an option: ") 
-    if choice == "1" :
+    choice = input("Choose an option: ")
+    if choice == "0" :
+        print("Goodbye!")
+        exit = True
+
+    elif choice == "1" :
         student_id = input("Enter Student ID: ")
-        name = input("Enter Name: ")
+        name = input("Enter Name: ").title()
         email = input("Enter Email: ")
         new_student = Student(student_id,name,email)
         gradebook.add_student(new_student)
         print("Student added successfully!")
-    elif choice == "0" :
-        print("Goodbye!")
-        exit = True
+       
     elif choice == "2" :
         for student in gradebook.students.values() :
             student.display_info()
+
     elif choice == "3" :
         course_code = input("Enter Course Code : ")
         course_name = input("Enter Course Name: ")
@@ -39,3 +42,36 @@ while not exit :
         gradebook.add_course(new_course)
         print("Course added successfully!")
 
+    elif choice == "4" :
+        student_id = input("Enter Student ID: ")
+        course_code = input("Enter Course Code : ")
+        gradebook.enroll_student(student_id,course_code)
+        print("Student enrolled successfully!")
+
+    elif choice == "5" :
+        course_code = input("Enter Course Code : ")
+        assessment_type = input("Enter Assessment Type (Quiz/Exam/Project): ").lower()
+        title = input("Enter Assessment Title: ")
+        max_score = int(input("Enter Max Score: "))
+        if assessment_type == "quiz" :
+            new_assessment = Quiz(title,max_score)
+        elif assessment_type == "exam" :
+            new_assessment = Exam(title,max_score)
+        elif assessment_type == "project" :
+            new_assessment = Project(title,max_score)
+        gradebook.add_assessment(course_code,new_assessment)
+        print("Assessment added successfully!")
+
+    elif choice == "6" :
+        student_id = input("Enter Student ID: ")
+        course_code = input("Enter Course Code : ")
+        title = input("Enter Assessment Title: ")
+        score = int(input("Enter Score: "))
+        gradebook.record_grade(student_id,course_code,title,score)
+        print("Grade recorded successfully!")
+        
+    elif choice == "7" :
+        student_id = input("Enter Student ID: ")
+        gradebook.show_report(student_id)
+
+    
